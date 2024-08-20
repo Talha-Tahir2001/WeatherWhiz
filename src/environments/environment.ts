ad1963ca24c93668486d2c072cd1e1aa7a262745
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2024-08-21 01:35:45 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    weatherApiBaseUrl: 'YOUR_BASE_URL',
+    weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather',
     XRapidAPIHostHeaderName: 'YOUR_API_HOST_HEADER_NAME',
     XRapidAPIHostHeaderValue: 'YOUR_API_HOST_HEADER_VALUE',
     XRapidAPIKeyHeaderName: 'YOUR_API_KEY_HEADER_NAME',
