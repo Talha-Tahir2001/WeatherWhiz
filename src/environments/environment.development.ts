598b0d73e8de07a1bdf6d8587eaef6f6d627c067
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2024-08-21 00:12:18 +0500

    Add Source Code

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'YOUR_BASE_URL',
+    XRapidAPIHostHeaderName: 'YOUR_API_HOST_HEADER_NAME',
+    XRapidAPIHostHeaderValue: 'YOUR_API_HOST_HEADER_VALUE',
+    XRapidAPIKeyHeaderName: 'YOUR_API_KEY_HEADER_NAME',
+    XRapidAPIKeyHeaderValue: 'YOUR_API_KEY_HEADER_VALUE'
+
+};
